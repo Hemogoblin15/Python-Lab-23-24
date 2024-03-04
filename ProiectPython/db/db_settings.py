2692d3644bc3ce997105c21366295549511da0be
Mihail Ghidoarcă <87978047+Hemogoblin15@users.noreply.github.com>
2024-03-04 21:43:04 +0200

    Delete ProiectPython directory

--- ProiectPython/db/db_settings.py
+++
@@ -1,9 +0,0 @@
-"""
-A dictionary holding parameters used for the database connection.
-"""
-postgresql = {
-    'pguser': 'postgres',
-    'pgpasswd': '123456',
-    'pghost': '127.0.0.1',
-    'pgport': 5432,
-    'pgdb': 'bingewatch'}
